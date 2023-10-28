b18b8210c9e1cb13eedc4f9d84203dbf5f1df8b1
demianAdli <alireza.adli4@gmail.com>
2023-10-28 10:02:21 -0400

    Exercise 5-1-3

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 import Button from "./components/Button/Button.tsx";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
   return (
     <div>
-      {pizza.toppings}
+      {cart.items[0].quantity}
       <br />
       <Button color="primary" onClick={handleClick}>
         My Button
